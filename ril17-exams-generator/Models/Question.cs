5880ce1854b89cfd351946b972ea1aa8e137bc85
zGul <a.schall@francemel.fr>
2019-02-19 15:49:16 +0100

    Question class added

---
+++ ril17-exams-generator/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ril17ExamsGenerator.Models
+{
+    public abstract class Question
+    {
+        public int ID { set; get; }
+
+        [Required]
+        public string question { set; get; }
+    }
+}

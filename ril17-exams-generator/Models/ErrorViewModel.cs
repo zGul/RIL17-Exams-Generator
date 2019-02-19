92fc27c32d38ceeed1aabcea15d8884035fd9b52
zGul <a.schall@francemel.fr>
2019-02-19 11:29:51 +0100

    Initial commit

---
+++ ril17-exams-generator/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ril17_exams_generator.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
